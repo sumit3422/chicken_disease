78ba7abd650684a07dce23c3e9291cf085a3b508
Sumit <sumitkhade362@gmail.com>
2024-10-21 03:09:58 +0530

    data ingestion completed

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_files= [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
-    "params.ysml",
+    "params.yaml",
     "requitenents.txt",
     "setup.py",
     "research/trails.ipynb",
